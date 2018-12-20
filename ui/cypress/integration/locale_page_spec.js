8b3471d89f0e130febc86c0bf094adb3168d4f19
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2018-12-20 18:57:58 -0500

    Move Cypress files

--- ui/cypress/integration/locale_page_spec.js
+++
@@ -1,7 +0,0 @@
-describe('Locale page', function () {
-    it('contains test locales', function () {
-        cy.visit('/locale');
-        cy.contains('Locales');
-        cy.get('[data-cy="locale-select"]').parent().click();
-    });
-});
