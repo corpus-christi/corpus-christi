e6f87b46350439f67b313f24333bd4665694c5d8
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2018-12-31 00:44:29 -0500

    Update UI to send JWT only when required
    - Now that the API is keen on this.

--- ui/src/plugins/axios.js
+++ ui/src/plugins/axios.js
@@ -1,9 +1,18 @@
 import Vue from "vue";
 import axios from "axios";
 
-Vue.prototype.$http = axios;
+const authAxios = axios.create({
+  baseUrl: "/"
+  // headers: { "Authorization": "Bearer NOT SET" }
+});
+Vue.prototype.$http = authAxios;
 
 export function setJWT(jwt) {
   const access_token = `Bearer ${jwt}`;
-  axios.defaults.headers.common["Authorization"] = access_token;
+  authAxios.defaults.headers.common["Authorization"] = access_token;
 }
+
+const plainAxios = axios.create({
+  baseUrl: "/"
+});
+Vue.prototype.$httpNoAuth = plainAxios;
