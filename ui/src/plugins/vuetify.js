65e8d9772bd922af336a2a4a78c550fe772f74ac
Edric Yu <yu.edric@gmail.com>
2019-01-08 13:11:18 -0500

    bundle icons in project

--- ui/src/plugins/vuetify.js
+++ ui/src/plugins/vuetify.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 import "vuetify/src/stylus/app.styl";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 Vue.use(Vuetify, {
-  iconfont: "md"
+  iconfont: "mdi"
 });
