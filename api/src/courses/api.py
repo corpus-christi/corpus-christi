9a37897bc79070658f9b50593e7b68f26e5594dc
Ryan Jones <ryan_jones5@taylor.edu>
2019-01-08 14:22:36 -0500

    Created Courses Boilerplate file for Course, Prereq, and Course_Offering tables

---
+++ api/src/courses/api.py
@@ -0,0 +1,11 @@
+from flask import request, jsonify
+from flask_jwt_extended import jwt_required
+from marshmallow import ValidationError
+
+from . import courses
+from .models import 
+from .. import db
+
+# ---- Course
+
+
