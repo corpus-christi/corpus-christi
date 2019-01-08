13324be628195619ac9375fbd945ef7387bdc3d3
Connor Wagner <connorawagner@icloud.com>
2019-01-08 13:26:59 -0500

    Added imports to resolve runtime errors

--- api/src/events/__init__.py
+++ api/src/events/__init__.py
@@ -3,7 +3,11 @@ from flask import Blueprint
 events = Blueprint('events', __name__)
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 #from . import api
+=======
+from . import api
+>>>>>>> Added imports to resolve runtime errors
 =======
 from . import api
 >>>>>>> Added imports to resolve runtime errors
