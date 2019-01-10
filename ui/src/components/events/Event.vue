1b516c8502b8857cf2ba2c189bcee8eb386a656f
Joey Ferguson <joey.ferguson2@gmail.com>
2019-01-10 15:45:25 -0500

    Added event details card layout (fake data), translations

--- ui/src/components/events/Event.vue
+++ ui/src/components/events/Event.vue
@@ -2,16 +2,16 @@
     <div>
         <v-tabs color="transparent" slider-color="accent">
             <v-tab ripple v-on:click="$router.push({path: '/events/' + $route.params.event + '/details'})">
-                <v-icon>list</v-icon>&nbsp;Details
+                <v-icon>list</v-icon>&nbsp;{{ $t('events.details') }}
             </v-tab>
             <v-tab ripple v-on:click="$router.push({path: '/events/' + $route.params.event + '/participants'})">
-                <v-icon>person </v-icon>&nbsp;Participants
+                <v-icon>person </v-icon>&nbsp;{{ $t('events.participants') }}
             </v-tab>
             <v-tab ripple v-on:click="$router.push({path: '/events/' + $route.params.event + '/teams'})">
-                <v-icon>group</v-icon>&nbsp;Teams
+                <v-icon>group</v-icon>&nbsp;{{ $t('events.teams') }}
             </v-tab>
             <v-tab ripple v-on:click="$router.push({path: '/events/' + $route.params.event + '/assets'})">
-                <v-icon>devices_other</v-icon>&nbsp;Assets
+                <v-icon>devices_other</v-icon>&nbsp;{{ $t('events.assets') }}
             </v-tab>
         </v-tabs>
         <hr class="vertical-spacer"/>
--- ui/src/components/events/EventDetails.vue
+++ ui/src/components/events/EventDetails.vue
@@ -1,9 +1,40 @@
 <template>
-    <div>
-        <v-card>
-        details
-        </v-card>
-    </div>
+  <v-layout>
+    <v-flex xs12 sm12>
+      <v-card>
+        <v-container fill-height fluid>
+            <v-flex xs12 sm12 align-end flexbox>
+              <span class="headline">Youth Spaghetti Dinner</span>
+            </v-flex>
+        </v-container>
+        <v-card-text>
+          <div>
+            <span><b>Location: </b>Coffee Dei</span><br>
+            <span><b>Date: </b>1/24/2019</span><br>
+            <span><b>Time: </b>6:00 PM - 7:30 PM</span><br><br>  
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer suscipit id turpis sed hendrerit. 
+              In hac habitasse platea dictumst. Aenean ut scelerisque purus. Phasellus a pretium tortor. 
+              Nunc et risus eu risus tempus placerat. Pellentesque habitant morbi tristique senectus et netus et 
+              malesuada fames ac turpis egestas. Sed pretium imperdiet aliquam. In cursus aliquet mi at gravida. 
+              Integer mollis, odio in viverra imperdiet, libero tortor bibendum dui, sit amet congue odio nisl a tortor. 
+              Sed suscipit rutrum elit et tincidunt. In egestas sem a sapien pharetra ullamcorper. Maecenas ut sagittis dui. 
+              Phasellus vitae tincidunt neque, eu convallis ante. </span><br>
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn flat color="primary" v-on:click="$router.push({path: '/events/' + $route.params.event + '/participants'})">
+            <v-icon>person</v-icon>&nbsp;{{ $t('events.participants') }}
+          </v-btn>
+          <v-btn flat color="primary" v-on:click="$router.push({path: '/events/' + $route.params.event + '/teams'})">
+            <v-icon>group</v-icon>&nbsp;{{ $t('events.teams') }}
+          </v-btn>
+          <v-btn flat color="primary" v-on:click="$router.push({path: '/events/' + $route.params.event + '/assets'})">
+            <v-icon>devices_other</v-icon>&nbsp;{{ $t('events.assets') }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
