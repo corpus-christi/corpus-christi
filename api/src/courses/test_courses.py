d9d52a45c2c98f2164886ffe01af3173bb16f819
Josh James <joshua_james1@taylor.edu>
2019-01-10 13:44:33 -0500

    Fixed bug that occured when populating prerequisite table

--- api/src/courses/test_courses.py
+++ api/src/courses/test_courses.py
@@ -64,13 +64,13 @@ def prerequisite_object_factory(course_id, prereq_id):
     return prerequisites
 
 
-def create_multiple_prerequisites(sqla, n=3):
+def create_multiple_prerequisites(sqla):
     """Commits the number of prerequisites to the DB."""
     courses = sqla.query(Course).all()
     prerequisite_schema = PrerequisiteSchema()
     new_prerequisites = []
-    for i in range(courses.count()-1):
-        valid_prerequisites = prerequisites_schema.load(prerequisite_object_factory(courses[i].id, courses[i+1].id))
+    for i in range(len(courses)-1):
+        valid_prerequisites = prerequisite_schema.load(prerequisite_object_factory(courses[i].id, courses[i+1].id))
         new_prerequisites.append(Prerequisite(**valid_prerequisites))
     sqla.add_all(new_prerequisites)
     sqla.commit()
