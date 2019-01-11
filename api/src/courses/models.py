68191b6f677a53236d74273552edf16447900723
Josh James <joshua_james1@taylor.edu>
2019-01-11 11:24:21 -0500

    Added many to many relationship between Class_Meeting and Student tables

--- api/src/courses/models.py
+++ api/src/courses/models.py
@@ -24,10 +24,35 @@ Diploma_Course = Table('courses_diploma_course', Base.metadata,
           Column('course_id', Integer, ForeignKey('courses_course.id'), primary_key=True),
           Column('diploma_id', Integer, ForeignKey('courses_diploma.id'), primary_key=True))
 
+
 class Diploma_CourseSchema(Schema):
      course_id = fields.Integer(dump_only=True, data_key='courseId', required=True)
      diploma_id = fields.Integer(dump_only=True, data_key='diplomaId', required=True)
 
+# ---- Diploma_Awarded
+
+Diploma_Awarded = Table('courses_diploma_awarded', Base.metadata,
+          Column('student_id', Integer, ForeignKey('courses_students.id'), primary_key=True),
+          Column('diploma_id', Integer, ForeignKey('courses_diploma.id'), primary_key=True),
+          Column('when', Date, nullable=False))
+
+
+class Diploma_AwardedSchema(Schema):
+     student_id = fields.Integer(dump_only=True, data_key='studentId', required=True, validate=Range(min=1))
+     diploma_id = fields.Integer(dump_only=True, data_key='diplomaId', required=True, validate=Range(min=1))
+     when = fields.Date(required=True)
+
+# ---- Class_Attendance
+
+Class_Attendance = Table('courses_class_attendance', Base.metadata,
+          Column('class_id', Integer, ForeignKey('courses_class_meeting.id'), primary_key=True),
+          Column('student_id', Integer, ForeignKey('courses_students.id'), primary_key=True))
+
+
+class Class_AttendanceSchema(Schema):
+    class_id = fields.Integer(dump_only=True, data_key='classId', required=True)
+    student_id = fields.Integer(dump_only=True, data_key='studentId', required=True)
+     
 # ---- Course
 
 class Course(Base):
@@ -58,18 +83,6 @@ class CourseSchema(Schema):
      description = fields.String(required=True, validate=Length(min=1))
      active = fields.Boolean(required=True)
 
-# ---- Diploma_Awarded
-
-Diploma_Awarded = Table('courses_diploma_awarded', Base.metadata,
-          Column('student_id', Integer, ForeignKey('courses_students.id'), primary_key=True),
-          Column('diploma_id', Integer, ForeignKey('courses_diploma.id'), primary_key=True),
-          Column('when', Date, nullable=False))
-
-
-class Diploma_AwardedSchema(Schema):
-     student_id = fields.Integer(dump_only=True, data_key='studentId', required=True, validate=Range(min=1))
-     diploma_id = fields.Integer(dump_only=True, data_key='diplomaId', required=True, validate=Range(min=1))
-     when = fields.Date(required=True)
 
 # ---- Diploma
 
@@ -106,7 +119,9 @@ class Student(Base):
      course_offering = relationship('Course_Offering', backref='offerings', lazy=True)
      person = relationship('Person', backref='students', lazy=True)
      diploma = relationship('Student', secondary=Diploma_Awarded,
-     backref='diplomas', lazy=True)
+               backref='diplomas', lazy=True)
+     class_meeting = relationship('Class_Meeting', secondary=Class_Attendance,
+               backref='attendance', lazy=True)
 
 
      def __repr__(self):
@@ -142,41 +157,27 @@ class Course_OfferingSchema(Schema):
 
 # ---- Class_Meeting
 
-# class Class_Meeting(Base):
-#      __tablename__ = 'courses_class_meeting'
-#      id = Column(Integer, primary_key=True)
-#      offering_id = Column(Integer, ForeignKey('courses_course_offering.id'), nullable=False)
-#      location = Column(Integer, ForeignKey('places_location.id'), nullable=False)
-#      teacher = Column(Integer, ForeignKey('people_person.id'), nullable=False)
-#      when = Column(DateTime, nullable=False)
-#      course_offering = relationship('Course_Offering', backref='class_meeting', lazy=True)
-#      location = relationship('Location', backref='meeting_location', lazy=True)
-#      person = relationship('Person', backref='teacher', lazy=True)
-#
-#      def __repr__(self):
-#          return f"<Class_Meeting(id={self.id})>"
+class Class_Meeting(Base):
+     __tablename__ = 'courses_class_meeting'
+     id = Column(Integer, primary_key=True)
+     offering_id = Column(Integer, ForeignKey('courses_course_offering.id'), nullable=False)
+     location = Column(Integer, ForeignKey('places_location.id'), nullable=False)
+     teacher = Column(Integer, ForeignKey('people_person.id'), nullable=False)
+     when = Column(DateTime, nullable=False)
+     course_offering = relationship('Course_Offering', backref='class_meeting', lazy=True)
+     #location = relationship('Location', backref='meeting_location', lazy=True)
+     person = relationship('Person', backref='teacher', lazy=True)
+     student = relationship('Student', secondary=Class_Attendance,
+               backref='Students', lazy=True)
+
+     def __repr__(self):
+         return f"<Class_Meeting(id={self.id})>"
 
 
-# class Class_MeetingSchema(Schema):
-#     id = fields.Integer(dump_only=True, required=True, validate=Range(min=1))
-#     offering_id = fields.Integer(data_key='offeringId', required=True)
-#     location = fields.Integer(required=True)
-#     teacher = fields.Integer(required=True)
-#     when = fields.DateTime(required=True)
+class Class_MeetingSchema(Schema):
+    id = fields.Integer(dump_only=True, required=True, validate=Range(min=1))
+    offering_id = fields.Integer(data_key='offeringId', required=True)
+    location = fields.Integer(required=True)
+    teacher = fields.Integer(required=True)
+    when = fields.DateTime(required=True)
 
-# ---- Class_Attendance
-
-# class Class_Attendance(Base):
-#     __tablename__ = 'courses_class_attendance'
-#     class_id = Column(Integer, ForeignKey('courses_class_meeting.id'), primary_key=True)
-#     student_id = Column(Integer, ForeignKey('courses_students.id'), primary_key=True)
-#     lecture = relationship('Class_Meeting', backref='attendance', foreign_keys=[class_id], lazy=True)
-#     students = relationship('Student', backref='students', foreign_keys=[student_id], lazy=True)
-#
-#     def __repr__(self):
-#         return f"<Class_Attendance(class_id={self.class_id},student_id={self.student_id})>"
-#
-#
-# class Class_AttendanceSchema(Schema):
-#     class_id = fields.Integer(dump_only=True, data_key='classId', required=True)
-#     student_id = fields.Integer(dump_only=True, data_key='studentId', required=True)
