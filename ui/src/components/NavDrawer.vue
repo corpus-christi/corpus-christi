88e4d4376bad72442906ffcef93e8ce7358d67ff
ImaniMuya <imani.muya@gmail.com>
2019-01-11 14:52:15 -0500

    ready to merge

--- ui/src/components/NavDrawer.vue
+++ ui/src/components/NavDrawer.vue
@@ -2,19 +2,19 @@
   <v-navigation-drawer app v-model="drawerVisible" width="200">
     <v-list>
       <template v-for="item in menuItems">
+        
         <v-list-tile v-bind:key="item.route" v-bind:to="{ name: item.route }">
           <v-list-tile-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
           <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          <v-list-tile-content>
-            <v-icon>{{ home }}</v-icon>
-          </v-list-tile-content>
         </v-list-tile>
+
         <v-divider
           v-if="item.divider"
           v-bind:key="'div-' + item.route"
         ></v-divider>
+
       </template>
     </v-list>
   </v-navigation-drawer>
