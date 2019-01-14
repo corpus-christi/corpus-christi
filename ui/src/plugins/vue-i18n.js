52fea870ec3843b548035e9535c085b08ee0485b
joshbontrager <joshua_bontrager@taylor.edu>
2019-01-14 15:10:47 -0500

    Add validation messages and vee-validation config

--- ui/src/plugins/vue-i18n.js
+++ ui/src/plugins/vue-i18n.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
-import VeeValidate from "vee-validate";    
+import VeeValidate from "vee-validate";
 import VueI18n from "vue-i18n";
+import enValidation from "vee-validate/dist/locale/en";
+import esValidation from "vee-validate/dist/locale/es";
 
 import i18n_data from "../../i18n/cc-i18n.json";
 
@@ -8,11 +10,23 @@ Vue.use(VueI18n);
 
 const i18n = new VueI18n({
   locale: "es",
-  messages: i18n_data
+  messages: i18n_data,
+  silentTranslationWarn: true
 });
 
 Vue.use(VeeValidate, {
-  i18n
+  i18n,
+  i18nRootKey: "i18n_data",
+  dictionary: {
+    en: {
+      messages: enValidation.messages,
+      attributes: i18n_data.en.validation.attributes
+    },
+    es: {
+      messages: esValidation.messages,
+      attributes: i18n_data.es.validation.attributes
+    }
+  }
 });
 
 export default i18n;
