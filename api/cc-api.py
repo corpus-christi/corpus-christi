f8dee4c6b627994a06a1a0ef2a04ae81fbf3f88d
Brandon George <geobrandon96@gmail.com>
2019-01-14 09:57:00 -0500

    added starters for making the class meetings

--- api/cc-api.py
+++ api/cc-api.py
@@ -13,7 +13,7 @@ from src.places.models import Country
 from src.courses.models import Course, Prerequisite
 from src.courses.test_courses import create_multiple_courses,\
     create_multiple_course_offerings, create_multiple_prerequisites,\
-    create_multiple_diplomas, create_multiple_students
+    create_multiple_diplomas, create_multiple_students, create_class_meetings
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
@@ -58,6 +58,7 @@ def load_languages():
     create_multiple_prerequisites(db.session)
     create_multiple_diplomas(db.session, 30)
     create_multiple_students(db.session, 30)
+    # create_class_meetings(db.session, 30)
 
 
 
