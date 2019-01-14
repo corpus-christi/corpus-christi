85f519b3a546ee96fb933c564d3bec97adbc1607
Noahffiliation <nlindsey15@gmail.com>
2019-01-14 10:00:33 -0500

    Update tests with TODOs to come back to later

--- ui/tests/e2e/specs/events/update_event_spec.js
+++ ui/tests/e2e/specs/events/update_event_spec.js
@@ -1,4 +1,4 @@
-// TODO: Wait for UI to finish with updating events
+// TODO: Skeleton done for now, should do more extensive testing later
 describe('Create Event Test', function() {
   it('GIVEN: Event Planner goes to Event page', function() {
     cy.visit('/login');
@@ -13,10 +13,16 @@ describe('Create Event Test', function() {
     cy.get('[data-cy=edit]').eq(0).click();
     cy.get('[data-cy=title]').type(' V2');
     
+    // Rewrite a new description of the event
+    // TODO: Check for updated description
     cy.get('[data-cy=description]').clear();
     cy.get('[data-cy=description]').type('Join us for a whole day of prayer.');
+
+    cy.get('[data-cy=form-save]').click();
   });
 
   it('THEN: Event details should be updated', function() {
+    // Check for new title in table
+    cy.get('tbody').contains(' V2');
   });
 });
