0c26bfd315e37f6a4146a13520b2deb5360e2ca7
Noahffiliation <nlindsey15@gmail.com>
2019-01-15 15:12:02 -0500

    Test for blank event form

--- ui/tests/e2e/specs/events/create_event_spec.js
+++ ui/tests/e2e/specs/events/create_event_spec.js
@@ -13,7 +13,6 @@ describe("Create Event Test", function() {
 
     cy.get("[data-cy=description]").type("A neat description of something.");
 
-    // TODO: Make API calls for test data???
     // Get first location in the search
     cy.get("[data-cy=entity-search-field]").click();
     cy.get(".menuable__content__active > .v-select-list > .v-list > :nth-child(1) > .v-list__tile").click();
@@ -46,24 +45,38 @@ describe("Create Event Test", function() {
     cy.get(".v-time-picker-clock__inner")
       .eq(1)
       .click();
-
     cy.get("[data-cy=end-time-ok]").click();
+
     cy.get("[data-cy=form-save]").click();
   });
 
-  // TODO: Get new event by ID
   it("THEN: A new event is listed in the table", function() {
+    // Switch to see all events on one page
     cy.get(
       ".v-datatable__actions__select > .v-input > .v-input__control > .v-input__slot > .v-select__slot"
     ).click();
     cy.contains("Todos").click();
+
     cy.get("tbody").contains("New Event");
   });
 
-  it("WHEN: Event planner adds a new event and fills out the details incorrectly", function() {
-    // TODO: No title
-    // TODO: No location
-    // TODO: Pick end date first
-    // TODO: Pick end time first
+  it("WHEN: Event planner adds a new event and fills out a blank form", function() {
+    cy.get("[data-cy=add-event]").click();
+    cy.get("[data-cy=form-save]").click();
   });
+    
+  it("THEN: Error messages appear", function() {
+    // Check for error messages under each input field
+    cy.get("form > :nth-child(1) > .v-input__control > .v-text-field__details > .v-messages > .v-messages__wrapper > .v-messages__message").should("exist");
+    cy.get("[name='location'] > .v-input > .v-input__control > .v-text-field__details > .v-messages > .v-messages__wrapper > .v-messages__message").should("exist");
+    cy.get("[data-cy=start-date-menu] > .v-menu__activator > .v-input > .v-input__control > .v-text-field__details > .v-messages > .v-messages__wrapper > .v-messages__message").should("exist");
+    cy.get("[data-cy=start-time-dialog] > .v-dialog__activator > .v-input > .v-input__control > .v-text-field__details > .v-messages > .v-messages__wrapper > .v-messages__message").should("exist");
+    cy.get("[data-cy=start-time-dialog] > .v-dialog__activator > .v-input > .v-input__control > .v-text-field__details > .v-messages > .v-messages__wrapper > .v-messages__message").should("exist");
+    cy.get("[data-cy=end-date-menu] > .v-menu__activator > .v-input > .v-input__control > .v-text-field__details > .v-messages > .v-messages__wrapper > .v-messages__message").should("exist");
+    cy.get("[data-cy=end-time-dialog] > .v-dialog__activator > .v-input > .v-input__control > .v-text-field__details > .v-messages > .v-messages__wrapper > .v-messages__message").should("exist");
+
+    cy.get("[data-cy=form-cancel]").click();
+  });
+  // TODO: Pick end date first
+  // TODO: Pick end time first
 });
