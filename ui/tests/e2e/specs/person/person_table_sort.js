41808069fb4913243515901c710aa8b7d70f67c6
andrewdorn1 <andrew_dorn@taylor.edu>
2019-01-15 11:07:58 -0500

    Finished the test that changes the person_table's display based on ascending/descending/id order based on a category(first,last,email,phone)

--- ui/tests/e2e/specs/person/person_table_sort.js
+++ ui/tests/e2e/specs/person/person_table_sort.js
@@ -19,26 +19,35 @@ describe("Testing Editing User Information", () => {
     //Making sure we're in the right place
     cy.url().should('include', '/admin');
     //open nav drawer
-    cy.get('[data-cy=open-navigation]')
+    cy.get('[data-cy=toggle-nav-drawer]')
       .click();
     //goes to the people page
     cy.get('[data-cy=people]')
       .click();
   });
   it("When: clicking on a sorting method: last name descending", () => {
-    cy.get('thead > :nth-child(1) > :nth-child(2)').click();
+    cy.get('thead > :nth-child(1) > :nth-child(3)').click();
   });
   it("Then: The table should resort itself based on what was being sorted", () => {
-    cy.get('tbody > :nth-child(1) > :nth-child(2)').contains('Assurance');
+    cy.get('tbody > :nth-child(1) > :nth-child(3)').contains('Altamirano');
   });
 
   it("And: When clicked again should change the sorting method: last name ascending", () => {
-    cy.get('thead > :nth-child(1) > :nth-child(2)').click();
-    cy.get('tbody > :nth-child(1) > :nth-child(2)').contains('Ziffle');
+    cy.get('thead > :nth-child(1) > :nth-child(3)').click();
+    cy.get('tbody > :nth-child(1) > :nth-child(3)').contains('Ziffle');
   });
   it("And: When clicked again should change the sorting method: last name by user_id", () => {
+    cy.get('thead > :nth-child(1) > :nth-child(3)').click();
+    cy.get('tbody > :nth-child(1) > :nth-child(3)').contains('Godoy');
+  });
+  it("And: The other sorting buttons should be clickable and work as well", () => {
     cy.get('thead > :nth-child(1) > :nth-child(2)').click();
-    cy.get('tbody > :nth-child(1) > :nth-child(2)').contains('Phillips');
+    cy.get('tbody > :nth-child(1) > :nth-child(2)').contains('Alberto');
+    cy.get('thead > :nth-child(1) > :nth-child(4)').click().click();
+    cy.get('tbody > :nth-child(1) > :nth-child(4)').contains('rodolfovela@industrias.net');
+    cy.get('thead > :nth-child(1) > :nth-child(5)').click();
+    cy.get('tbody > :nth-child(1) > :nth-child(5)').should('be.empty');
+
   });
 
 });
