67e5c9123c8a94ffa45b2d4ab232eb7dd0b4bf0e
Jacob Banks <jacobanks@gmail.com>
2019-01-16 15:32:20 -0500

    CourseOfferingStudents

--- ui/src/components/courses/CourseOfferingDetails.vue
+++ ui/src/components/courses/CourseOfferingDetails.vue
@@ -5,20 +5,14 @@
         <template v-if="pageLoaded">
           <v-container fill-height fluid>
             <v-flex xs9 sm9 align-end flexbox>
-              <span class="headline">{{ courseOffering.title }}</span>
+              <span class="headline">{{ courseName }}</span>
             </v-flex>
-            <v-layout xs3 sm3 align-end justify-end>
-              <v-btn flat color="primary" v-on:click="editCourseOffering(courseOffering)">
-                <v-icon>edit</v-icon>&nbsp;{{ $t("actions.edit") }}
-              </v-btn>
-            </v-layout>
           </v-container>
           <v-card-text class="pa-4">
-            <div>
-              <b>Location: </b>
-              <div class="multi-line ml-2">{{ displayLocation }}</div>
-            </div>
-            <div class="mt-2">{{ courseOffering.description }}</div>
+            <b>{{ $t("courses.description") }}:</b>
+            <div class="ml-2">{{ courseOffering.description }}</div>
+            <b>{{ $t("courses.enrolled") }}:</b>
+            <div class="ml-2">{{ "0 / " + courseOffering.maxSize }}</div>
           </v-card-text>
           <v-card-actions>
             <v-btn
@@ -41,32 +35,29 @@
         </v-layout>
       </v-card>
     </v-flex>
-
-    <!-- Edit dialog -->
-    <v-dialog persistent scrollable v-model="courseOfferingDialog.show" max-width="500px">
-      <CourseOfferingForm
-        v-bind:editMode="courseOfferingDialog.editMode"
-        v-bind:initialData="courseOfferingDialog.courseOffering"
-        v-bind:saving="courseOfferingDialog.saving"
-        v-on:cancel="cancelCourseOffering"
-        v-on:save="saveCourseOffering"
-      />
-    </v-dialog>
   </v-layout>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import CourseOfferingForm from "./CourseOfferingForm";
 
 export default {
   name: "CourseOfferingDetails",
+  components: {
+    CourseOfferingForm
+  },
 
   mounted() {
     this.pageLoaded = false;
     const id = this.offeringId;
     this.$http.get(`/api/v1/courses/course_offerings/${id}`).then(resp => {
       this.courseOffering = resp.data;
-      this.pageLoaded = true;
+      
+      this.$http.get(`/api/v1/courses/courses/${this.courseOffering.courseId}`).then(resp => {
+        this.courseName = resp.data.name;
+        this.pageLoaded = true;
+      });
     });
   },
 
@@ -78,58 +69,25 @@ export default {
     return {
       courseOffering: {},
       courseOfferingDialog: {
-        courseOffering: {},
         show: false,
-        saveLoading: false
+        editMode: false,
+        saving: false,
+        courseOffering: {}
       },
 
       snackbar: {
         show: false,
         text: ""
       },
-      pageLoaded: false
+      pageLoaded: false,
+      
+      courseName: ""
     };
   },
   props: {
     offeringId: 0
   },
   methods: {
-    editCourseOffering(courseOffering) {
-      this.courseOfferingDialog.courseOffering = JSON.parse(JSON.stringify(courseOffering));
-      this.courseOfferingDialog.show = true;
-    },
-
-    cancelCourseOffering() {
-      this.courseOfferingDialog.show = false;
-    },
-
-    saveCourseOffering(courseOffering) {
-      this.courseOfferingDialog.saving = true;
-      const courseOffering_id = courseOffering.id;
-
-      // Locate the record we're updating in the table.
-      const idx = this.courseOfferings.findIndex(c => c.id === courseOffering.id);
-      // Get rid of the ID; not for consumption by endpoint.
-      delete courseOffering.id;
-
-      this.$http
-        .patch(`/api/v1/courses/course_offerings/${courseOffering_id}`, courseOffering)
-        .then(resp => {
-          console.log("EDITED", resp);
-          Object.assign(this.courseOfferings[idx], courseOffering);
-          this.snackbar.text = this.$t("courses.updated");
-          this.snackbar.show = true;
-        })
-        .catch(err => {
-          console.error("FALURE", err.response);
-          this.snackbar.text = this.$t("courses.update-failed");
-          this.snackbar.show = true;
-        })
-        .finally(() => {
-          this.courseOfferingDialog.show = false;
-          this.courseOfferingDialog.saving = false;
-        });
-    },
 
     getDisplayDate(ts) {
       let date = new Date(ts);
