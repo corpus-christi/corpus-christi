0ca150384e53c7cc9f49efea0b4d5af4f4350a8e
David Fletcher <david_fletcher@taylor.edu>
2019-01-18 10:55:18 -0500

    add error message if incorrect login credentials

--- ui/src/pages/Login.vue
+++ ui/src/pages/Login.vue
@@ -1,47 +1,56 @@
 <template>
-  <v-container fluid fill-height>
-    <v-layout align-center justify-center>
-      <v-flex xs12 sm8 md6>
-        <v-card class="elevation-12">
-          <v-toolbar dark color="primary">
-            <v-toolbar-title>{{ $t("login.header") }}</v-toolbar-title>
-            <v-spacer></v-spacer>
-          </v-toolbar>
-          <v-card-text>
-            <v-form>
-              <v-text-field
-                v-model="username"
-                v-bind:label="$t('account.username')"
-                prepend-icon="person"
-                name="login"
-                type="text"
-                v-on:keyup.enter="login"
-                data-cy="username"
-              ></v-text-field>
-              <v-text-field
-                v-model="password"
-                v-bind:label="$t('account.password')"
-                prepend-icon="lock"
-                name="password"
-                type="password"
-                v-on:keyup.enter="login"
-                data-cy="password"
-              ></v-text-field>
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="primary" v-on:click="cancel" data-cy="cancel">
-              {{ $t("actions.cancel") }}
-            </v-btn>
-            <v-btn color="primary" v-on:click="login" data-cy="login">
-              {{ $t("actions.login") }}
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md6>
+          <v-card class="elevation-12">
+            <v-toolbar dark color="primary">
+              <v-toolbar-title>{{ $t("login.header") }}</v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  v-model="username"
+                  v-bind:label="$t('account.username')"
+                  prepend-icon="person"
+                  name="login"
+                  type="text"
+                  v-on:keyup.enter="login"
+                  data-cy="username"
+                ></v-text-field>
+                <v-text-field
+                  v-model="password"
+                  v-bind:label="$t('account.password')"
+                  prepend-icon="lock"
+                  name="password"
+                  type="password"
+                  v-on:keyup.enter="login"
+                  data-cy="password"
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" v-on:click="cancel" data-cy="cancel">
+                {{ $t("actions.cancel") }}
+              </v-btn>
+              <v-btn color="primary" v-on:click="login" data-cy="login">
+                {{ $t("actions.login") }}
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <v-snackbar v-model="snackbar.show">
+      {{ snackbar.text }}
+      <v-btn flat @click="snackbar.show = false" data-cy>{{
+        $t("actions.close")
+      }}</v-btn>
+    </v-snackbar>
+  </div>
 </template>
 
 <script>
@@ -53,7 +62,12 @@ export default {
   data() {
     return {
       username: "",
-      password: ""
+      password: "",
+
+      snackbar: {
+        show: false,
+        text: ""
+      },
     };
   },
   methods: {
@@ -64,28 +78,34 @@ export default {
     },
 
     async login() {
-      const resp = await this.$httpNoAuth.post("/api/v1/auth/login", {
-        username: this.username,
-        password: this.password
-      });
-      if (resp.status !== 200) {
-        console.error(`JWT STATUS ${resp.status}`);
-        return;
+      try {      
+        const resp = await this.$httpNoAuth.post("/api/v1/auth/login", {
+          username: this.username,
+          password: this.password
+        });
+        if (resp.status !== 200) {
+          console.error(`JWT STATUS ${resp.status}`);
+          return;
+        } else {
+          this.logIn({
+            account: new Account(
+            resp.data.username,
+            resp.data.firstName,
+            resp.data.lastName
+            ),
+            jwt: resp.data.jwt
+          });
+
+          // Normally want to use `push`, but unlikely that
+          // the user wants to return to the login page.
+          const routeName = this.$route.query.redirect || "admin";
+          this.$router.replace({ name: routeName });
+        }
+      } catch(err) {
+        console.log(err);
+        this.snackbar.text = this.$t("login.messages.incorrect-login");
+        this.snackbar.show = true;
       }
-
-      this.logIn({
-        account: new Account(
-          resp.data.username,
-          resp.data.firstName,
-          resp.data.lastName
-        ),
-        jwt: resp.data.jwt
-      });
-
-      // Normally want to use `push`, but unlikely that
-      // the user wants to return to the login page.
-      const routeName = this.$route.query.redirect || "admin";
-      this.$router.replace({ name: routeName });
     }
   }
 };
