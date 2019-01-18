57c705468bc1f528d0b029b2685e5250907f0781
Jacob Banks <jacobanks@gmail.com>
2019-01-18 14:40:14 -0500

    Fixed tooltips

--- ui/src/components/courses/actions/StudentsAdminActions.vue
+++ ui/src/components/courses/actions/StudentsAdminActions.vue
@@ -1,5 +1,19 @@
 <template>
   <v-layout align-center justify-end>
+    <v-tooltip bottom v-if="!student.confirmed">
+      <v-btn
+        flat
+        icon
+        outline
+        color="primary"
+        slot="activator"
+        v-bind:small="displayContext === 'compact'"
+        @click="emitAction('confirm')"
+      >
+        <v-icon v-bind:small="displayContext === 'compact'">done</v-icon>
+      </v-btn>
+      <span>{{ $t("actions.confirm") }}</span>
+    </v-tooltip>
     <v-tooltip bottom>
       <v-btn
         flat
@@ -14,21 +28,7 @@
           {{ student.active ? "archive" : "undo" }}
         </v-icon>
       </v-btn>
-    </v-tooltip>
-    <v-tooltip bottom v-if="!student.confirmed"
->
-      <v-btn
-        flat
-        icon
-        outline
-        color="primary"
-        slot="activator"
-        v-bind:small="displayContext === 'compact'"
-        @click="emitAction('confirm')"
-      >
-        <v-icon v-bind:small="displayContext === 'compact'">done</v-icon>
-      </v-btn>
-      <span>{{ $t(student.active ? "actions.tooltips.deactivate" : "actions.tooltips.reactivate") }}</span>
+      <span>{{ $t(student.active ? "actions.tooltips.archive" : "actions.tooltips.unarchive") }}</span>
     </v-tooltip>
   </v-layout>
 </template>
