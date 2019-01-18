9951f2815dfafcae987326f6c21846e7a3a8507a
andrewdorn1 <andrew_dorn@taylor.edu>
2019-01-18 10:56:48 -0500

    #189 renamed add_person test & made it much more robust deleted redundant test files that are included in other test files made edit_user test much more robust lightly touched up other tests

--- ui/tests/e2e/specs/person/change_people_view.js
+++ ui/tests/e2e/specs/person/change_people_view.js
@@ -1,5 +1,6 @@
 // Log in
-/*describe("Seed for test", () => {
+/*
+describe("Seed for test", () => {
   it("Given: seeding", function() {
     cy.exec("cd ../api && source ./set-up-bash.sh && ./reset-database.sh");
   });
@@ -19,7 +20,7 @@ describe("Tests the Person table Archived/active user filter", function() {
     });
 
 
-  it("When: The dropdown is clicked, and an archived users is selected",  function() {
+  it("When: The dropdown is clicked, and an archived users is selected", function() {
     cy.get("[data-cy=view-dropdown]").click(); // open dropdown
     let dropdown = ".menuable__content__active > .v-select-list > .v-list"; // path to dropdown child elements
     cy.get(dropdown).find(":nth-child(2)").first().click(); //selects view archived users
@@ -39,27 +40,5 @@ describe("Tests the Person table Archived/active user filter", function() {
     cy.get('tbody > :nth-child(1) > :nth-child(2)').should('not.contain',this.firstName);
     cy.get('[data-cy=reactivate-person').should('not.exist');
   });
-
-  /*it("Then: There should be zero archived users in the table", () => {
-    cy.get("tbody > :nth-child(1) > :nth-child(2)").should("not.exist");
-  });
-  it("And: When a user is archived, they should show up in the archived people", () => {
-    //go to active
-    cy.get("[data-cy=view-dropdown]").click(); // open dropdown
-    cy.get('@dropdown').find(":nth-child(1)")
-      .first().click(); // find and click child element in dropdown
-    //select first person & deactivate them
-    cy.get('tbody > :nth-child(1) > :nth-child(2)').as('firstName');
-    cy.get('tbody > :nth-child(1) [data-cy = deactivate-person]').click();
-    //make sure they are not there anymore
-    cy.get('tbody > :nth-child(1) > :nth-child(2)').should('not.contain','@firstName');
-
-    //go to archived users
-    //make sure they are there
-    //go to all users, make sure he is first user
-
-    //cy.get("tbody > :nth-child(1) > :nth-child(2)").should("not.exist");
-  });
-*/
 });
 
