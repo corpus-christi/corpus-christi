35929c6cf66dc6d4d611e184ecf0d22e9f3d9aec
Brandon George <geobrandon96@gmail.com>
2019-01-21 10:55:18 -0500

    squash! adding command line interfaces and tests

--- api/reset-database.sh
+++ api/reset-database.sh
@@ -10,4 +10,5 @@ flask db upgrade
 flask data load-all
 flask account new --first="Fred" --last="Ziffle" username password
 flask course new --prereq=6 --offering="Around happy fast" "Alone low investment" "blah blah blah"
+flask diploma new "Above" "blah blah blah"
 flask account new --first="Quality" --last="Assurance" Cytest password
