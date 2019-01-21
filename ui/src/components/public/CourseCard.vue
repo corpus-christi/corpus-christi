7190eee8527dffe0e1e2f2418aec345f39be31dd
ImaniMuya <imani.muya@gmail.com>
2019-01-21 10:30:08 -0500

    fix up

--- ui/src/components/public/CourseCard.vue
+++ ui/src/components/public/CourseCard.vue
@@ -1,28 +1,30 @@
 <template>
-  <div class="body">
-    <!-- Display course title, description and register button -->
-      <v-card-title>
-        <v-layout row align-center justify-center>
-          <v-flex shrink>
-            <span class="headline mb-3">{{ course.title }}</span>
-          </v-flex>
-        </v-layout>
-      </v-card-title>
+  <v-card>
+    <div class="body">
+      <!-- Display course title, description and register button -->
+        <v-card-title>
+          <v-layout row align-center justify-center>
+            <v-flex shrink>
+              <span class="headline mb-3">{{ course.title }}</span>
+            </v-flex>
+          </v-layout>
+        </v-card-title>
 
-      <v-card-text class="text">
-        <v-layout>
-          <v-flex>
-            <div class="mb-3">{{ course.description }}</div>
-          </v-flex>
-        </v-layout>
-      </v-card-text>
+        <v-card-text class="text">
+          <v-layout>
+            <v-flex>
+              <div class="mb-3">{{ course.description }}</div>
+            </v-flex>
+          </v-layout>
+        </v-card-text>
 
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn raised color="primary">{{ $t("courses.course.register") }}</v-btn>
-        <v-spacer></v-spacer>
-      </v-card-actions>
-    </div>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn raised color="primary">{{ $t("courses.course.register") }}</v-btn>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </div>
+    <v-card>
 </template>
 
 <script>
