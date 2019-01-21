6c6fa067f0c2c342013da3be97a8246c8e32dd29
ImaniMuya <imani.muya@gmail.com>
2019-01-21 11:41:23 -0500

    added base registration form

--- ui/src/components/public/CourseCard.vue
+++ ui/src/components/public/CourseCard.vue
@@ -1,42 +1,73 @@
 <template>
-  <v-card class="card elevation-10">
-    <div class="body">
+  <div>
+    <v-card class="card elevation-10">
       <!-- Display course title, description and register button -->
-        <v-card-title>
-          <v-layout row align-center justify-center>
-            <v-flex shrink>
-              <span class="headline mb-3">{{ course.title }}</span>
-            </v-flex>
-          </v-layout>
-        </v-card-title>
+      <v-card-title>
+        <v-layout row align-center justify-center>
+          <v-flex shrink>
+            <span class="headline mb-3">{{ course.name }}</span>
+          </v-flex>
+        </v-layout>
+      </v-card-title>
 
-        <v-card-text class="text">
-          <v-layout>
+      <div class="body">
+        <v-layout>
+          <v-card-text class="text">
             <v-flex>
               <div class="mb-3">{{ course.description }}</div>
             </v-flex>
-          </v-layout>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn raised color="primary">{{ $t("courses.course.register") }}</v-btn>
-          <v-spacer></v-spacer>
-        </v-card-actions>
+          </v-card-text>
+        </v-layout>
+        <v-layout>
+          <v-flex>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn raised color="primary" 
+               @click="registrationFormDialog.show = true">{{
+                $t("courses.course.register")
+              }}</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-flex>
+        </v-layout>
       </div>
     </v-card>
+    <v-dialog v-model="registrationFormDialog.show" max-width="500px">
+      <courseRegistrationForm 
+        v-on:cancel="cancel"
+        />
+    </v-dialog>
+  </div>
 </template>
 
 <script>
+
+import courseRegistrationForm from "./courseRegistrationForm";
+
 export default {
   name: "CourseCard",
-  props:{ 
+  props: {
     course: {}
   },
+  components: {
+    courseRegistrationForm
+  },
   data() {
-    return {};
+    return {
+      registrationFormDialog: {
+        show: false,
+        editMode: false,
+        saving: false,
+        courseOffering: {}
+      },
+    };
+  },
+
+  methods: {
+    cancel() {
+      this.registrationFormDialog.show = false;
+    },
   }
-  
 };
 </script>
 
---
+++ ui/src/components/public/courseRegistrationForm.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card>
+    <v-card-title>Register for this course</v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field
+          v-model="username"
+          v-bind:label="$t('account.username')"
+          prepend-icon="person"
+          type="text"
+          data-cy="username"
+        ></v-text-field>
+        <v-text-field
+          v-model="password"
+          v-bind:label="$t('account.password')"
+          prepend-icon="lock"
+          name="password"
+          type="password"
+          data-cy="password"
+        ></v-text-field>
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" v-on:click="cancel" data-cy="cancel">{{ $t("actions.cancel") }}</v-btn>
+      <v-btn color="primary" v-on:click="register" data-cy="register">{{ $t("actions.login") }}</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "courseRegistrationForm",
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  props: {
+  },
+
+  methods: {
+    cancel() {
+      this.clear();
+      this.$emit("cancel");
+    },
+    register() {
+
+    },
+    clear() {
+      
+    }
+  }
+};
+</script>
+
+<style>
+</style>
