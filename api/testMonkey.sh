2804242dea209d5ee094642e86af056c23c5c8ec
WKercher <willindiana@gmail.com>
2019-01-22 10:52:51 -0500

    full test coverage!!!!!!!!!!!! Whoot whoot yeet yeet. Ham sandwich. Fixed enumerated values schema and associated tests

--- api/testMonkey.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#pytest -x src/people/test_people.py
-#pytest src/people/test_people.py::test_activate_person_no_exist
-#pytest src/people/test_people.py::test_read_one_account
-#pytest src/people/test_people.py::test_remove_role_from_account
-pytest src/people/test_people.py --cov=src --cov-report=html
-
-
