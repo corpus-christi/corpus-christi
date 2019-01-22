e97aa0c24354f01513f1e02a607eb5c2fbe68d76
Jacob Banks <jacobanks@gmail.com>
2019-01-22 10:47:02 -0500

    Fixed getEventData function

--- ui/src/pages/Public.vue
+++ ui/src/pages/Public.vue
@@ -213,7 +213,17 @@ export default {
         })
         .catch(err => console.log("FAILED", err));
     },
-
+    
+    getEventData() {
+      this.pageLoaded = false;
+      this.$http.get(`/api/v1/events/?return_group=all`).then(resp => {
+        this.events = resp.data;
+        this.events = this.events.slice(0, 5);
+        console.log(resp.data);
+        this.pageLoaded = true;
+      });
+    },
+    
     cancel() {
       this.registrationFormDialog.show = false;
     },
