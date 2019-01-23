3ebfc721cea1e32a940680872ed1e7d618249db1
Nic Rose <nic_rose@taylor.edu>
2019-01-23 09:57:05 -0500

    THIS IS A HUGE HACK

---
+++ ui/eslintrc-ci.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/essential", "@vue/prettier"],
+  rules: {
+    "no-console": "off",
+    "no-debugger": "off"
+  },
+  parserOptions: {
+    parser: "babel-eslint"
+  }
+};
