a900cb3bed4206fba85f116939af0981ee6ca400
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2019-02-19 14:22:13 -0500

    Add private.py template

---
+++ api/private.py
@@ -0,0 +1,10 @@
+# Sample data; replace with your own settings
+#
+# DO NOT COMMIT YOUR SETTINGS TO VERSION CONTROL!
+
+PSQL_HOST = 'localhost'
+PSQL_USER = 'arco'
+PSQL_PASS = 'password'
+
+FLASK_SECRET_KEY = 'big secret'
+JWT_SECRET_KEY = 'super secret'
