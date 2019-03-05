652607e2bab6a3fd86d6f06e2cf7b1e1152cdcab
bromlu <lukewilliambrom@gmail.com>
2019-03-05 15:37:08 -0500

    Fix email tests

--- api/src/emails/test_emails.py
+++ api/src/emails/test_emails.py
@@ -1,28 +1,43 @@
 import pytest
 import random
-import datetime
-import random
 from faker import Faker
 from flask import url_for
-from flask_jwt_extended import create_access_token
-from werkzeug.datastructures import Headers
-from werkzeug.security import check_password_hash
 
-from .models import EmailSchema
-from ..events.create_event_data import flip, fake, create_multiple_events, event_object_factory, email_object_factory, create_multiple_assets, create_multiple_teams, create_events_assets, create_events_teams, create_events_persons, create_events_participants, create_teams_members, get_team_ids, asset_object_factory, team_object_factory
-from ..places.test_places import create_multiple_locations, create_multiple_addresses, create_multiple_areas
-from ..people.test_people import create_multiple_people
 
+class RandomLocaleFaker:
+    """Generate multiple fakers for different locales."""
+
+    def __init__(self, *locales):
+        self.fakers = [Faker(loc) for loc in locales]
+
+    def __call__(self):
+        """Return a random faker."""
+        return random.choice(self.fakers)
+
+
+rl_fake = RandomLocaleFaker('en_US', 'es_MX')
 fake = Faker()
 
-@pytest.mark.smoke
+
+def email_object_factory():
+    email = {
+        'subject': 'Test Email',
+        'body': rl_fake().sentences(nb=1)[0],
+        'recipients': ['tim_ours@taylor.edu']
+    }
+
+    return email
+
+
+# @pytest.mark.smoke
 def test_send_email(auth_client):
     # this test is intended to fail without proper credentials
 
     # GIVEN nothing
 
     # WHEN we try to send an email
-    resp = auth_client.post(url_for('emails.send_email'), json = email_object_factory())
+    resp = auth_client.post(url_for('emails.send_email'),
+                            json=email_object_factory())
 
     # THEN we expect the correct code
     assert resp.status_code == 200
@@ -35,8 +50,7 @@ def test_send_email_invalid(auth_client):
     # WHEN we try to send an invalid email
     email = email_object_factory()
     email[fake.word()] = fake.word()
-    resp = auth_client.post(url_for('emails.send_email'), json = email)
+    resp = auth_client.post(url_for('emails.send_email'), json=email)
 
     # THEN we expect an error code
     assert resp.status_code == 422
-
