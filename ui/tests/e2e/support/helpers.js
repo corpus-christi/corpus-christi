2d6b69c2a215af7f03a34beb8740773bea856b15
joshbontrager <josh.bontrager>
2019-04-24 16:34:18 -0400

    Add helpers.js which enables tests to use randomized strings

---
+++ ui/tests/e2e/support/helpers.js
@@ -0,0 +1,9 @@
+export function unique_id() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 10; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+};
