e603126146596149d649b127b2ad463efc7d35cc
Matt Hapner <matthew.hapner@gmail.com>
2019-05-10 15:23:42 -0400

    Added private.py back in

---
+++ api/private.py
@@ -0,0 +1,10 @@
+# Sample data; replace with your own settings
+#
+# DO NOT COMMIT PRODUCTION SETTINGS TO VERSION CONTROL!
+
+PSQL_HOST = '127.0.0.1'
+PSQL_USER = 'arco'
+PSQL_PASS = ''
+
+FLASK_SECRET_KEY = 'big secret'
+JWT_SECRET_KEY = 'super secret'
