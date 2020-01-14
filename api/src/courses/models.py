ba18dda2861d524a0b86baf3b17334379873a38f
Matt Dean <mdean400@gmail.com>
2020-01-14 10:47:09 -0500

    Reorder

--- api/src/courses/models.py
+++ api/src/courses/models.py
@@ -50,6 +50,26 @@ class PrerequisiteSchema(Schema):
     prereq_id = fields.Integer(
         dump_only=True, data_key='prereqId', required=True)
 
+# ---- DiplomaCourse
+
+
+class DiplomaCourse(Base):
+    __tablename__ = 'courses_diploma_course'
+    course_id = Column(Integer, ForeignKey(
+        'courses_course.id'), primary_key=True)
+    diploma_id = Column(Integer, ForeignKey(
+        'courses_diploma.id'), primary_key=True)
+
+    def __repr__(self):
+            return f"<DiplomaCourse(course_id={self.course_id},diploma_id={self.diploma_id})>"
+
+
+class DiplomaCourseSchema(Schema):
+    course_id = fields.Integer(
+        dump_only=True, data_key='courseId', required=True)
+    diploma_id = fields.Integer(
+        dump_only=True, data_key='diplomaId', required=True)
+
 # ---- Diploma
 
 
@@ -70,26 +90,6 @@ class DiplomaSchema(Schema):
     description = fields.String(required=True, validate=Length(min=1))
     active = fields.Boolean(required=True)
 
-
-# ---- DiplomaCourse
-
-
-class DiplomaCourse(Base):
-    __tablename__ = 'courses_diploma_course'
-    course_id = Column(Integer, ForeignKey(
-        'courses_course.id'), primary_key=True)
-    diploma_id = Column(Integer, ForeignKey(
-        'courses_diploma.id'), primary_key=True)
-
-    def __repr__(self):
-            return f"<DiplomaCourse(course_id={self.course_id},diploma_id={self.diploma_id})>"
-
-
-class DiplomaCourseSchema(Schema):
-    course_id = fields.Integer(
-        dump_only=True, data_key='courseId', required=True)
-    diploma_id = fields.Integer(
-        dump_only=True, data_key='diplomaId', required=True)
 
 # ---- DiplomaAwarded
 
