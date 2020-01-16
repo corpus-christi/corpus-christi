c3bde57d5ec0a391843630c0c5bd25a7de42a1b5
Elliot Wirrick <wirrickelliot@gmail.com>
2020-01-16 12:05:51 -0500

    fixed manager sort functionality on groups page

--- ui/src/components/groups/GroupTable.vue
+++ ui/src/components/groups/GroupTable.vue
@@ -265,7 +265,7 @@ export default {
       return [
         { text: this.$t("groups.name"), value: "name" },
         { text: this.$t("groups.description"), value: "description" },
-        { text: this.$t("groups.manager"), value: "managerInfo" },
+        { text: this.$t("groups.manager"), value: "managerInfo.person.lastName" },
         { text: this.$t("groups.member-count"), value: "memberList.length" },
         { text: this.$t("actions.header"), sortable: false }
       ];
