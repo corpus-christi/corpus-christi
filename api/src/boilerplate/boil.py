4d61ec1a924eb850386d7e9f546d768baf6615c8
Matt Dean <mdean400@gmail.com>
2020-01-17 14:07:47 -0500

    Make boil.py always output four spaces as tab instead of five sometimes, and make URI not a required attribute
    If URI is not provided, boil.py will not produce API code for that entity

--- api/src/boilerplate/boil.py
+++ api/src/boilerplate/boil.py
@@ -35,11 +35,11 @@ def camel_case(s):
 
 
 def tab(string, n=1):
-    print(" " * n * 4, string)
+    print(f'{" " * n * 4}{string}')
 
 
 def rule(n, char='='):
-    return char * n;
+    return char * n
 
 
 def banner(string, fat=False):
@@ -423,7 +423,7 @@ schema = {
                         }
                     }
                 },
-                "required": ["name", "singular", "plural", "uri", "table", "attributes"],
+                "required": ["name", "singular", "plural", "table", "attributes"],
                 "additionalProperties": False
             }
         }
@@ -457,7 +457,10 @@ for file_name in sys.argv[1:]:
     banner("APIs", True)
     for entity in entities:
         comment(entity['name'])
-        generate_api(module, entity)
+        if ('uri' in entity):
+            generate_api(module, entity)
+        else:
+            print("No URI found for this entity, so no API boilerplate produced.")
 
     banner("Tests", True)
     for entity in entities:
