48a76f326d302bbaf3574cf3995dd0aa338a24fe
David Deng <daviddenghaotian@163.com>
2020-05-28 16:04:14 -0400

    added factory functions for group_type and manager_type

--- api/src/groups/create_group_data.py
+++ api/src/groups/create_group_data.py
@@ -4,10 +4,9 @@ import random
 from faker import Faker
 
 from ..events.models import EventGroup, EventGroupSchema, Event
-from ..groups.models import Manager, Group, Meeting, Attendance, Member, GroupSchema, MeetingSchema, AttendanceSchema, \
-    MemberSchema
+from ..groups.models import Manager, ManagerType, Group, GroupType, Meeting, Attendance, Member, ManagerSchema, GroupSchema, MeetingSchema, AttendanceSchema, MemberSchema, GroupTypeSchema, ManagerTypeSchema
 from ..people.models import Person, Role, RoleSchema
-from ..people.test_people import create_multiple_managers, create_multiple_people
+from ..people.test_people import create_multiple_people
 from ..places.models import Address
 from ..places.test_places import create_multiple_addresses
 
@@ -35,25 +34,25 @@ def flip():
 
 def group_object_factory(sqla):
     """Cook up a fake group."""
-    all_managers = sqla.query(Manager).all()
-    if not all_managers:
-        create_multiple_managers(sqla, random.randint(3, 6))
-        all_managers = sqla.query(Manager).all()
+    # all_managers = sqla.query(Manager).all()
+    # if not all_managers:
+    #     create_multiple_managers(sqla, random.randint(3, 6))
+    #     all_managers = sqla.query(Manager).all()
     group = {
         'name': rl_fake().word(),
         'description': rl_fake().sentences(nb=1)[0],
         'active': flip(),
-        'managerId': all_managers[random.randint(0, len(all_managers) - 1)].id
+        # 'managerId': all_managers[random.randint(0, len(all_managers) - 1)].id
     }
     return group
 
 
 def group_object_factory_with_members(sqla, fraction=0.75):
     """Cook up a fake group."""
-    all_managers = sqla.query(Manager).all()
-    if not all_managers:
-        create_multiple_managers(sqla, random.randint(3, 6))
-        all_managers = sqla.query(Manager).all()
+    # all_managers = sqla.query(Manager).all()
+    # if not all_managers:
+    #     create_multiple_managers(sqla, random.randint(3, 6))
+    #     all_managers = sqla.query(Manager).all()
     all_people = sqla.query(Person).all()
     if not all_people:
         create_multiple_people(sqla, random.randint(3, 6))
@@ -62,7 +61,7 @@ def group_object_factory_with_members(sqla, fraction=0.75):
         'name': rl_fake().word(),
         'description': rl_fake().sentences(nb=1)[0],
         'active': flip(),
-        'managerId': all_managers[random.randint(0, len(all_managers) - 1)].id,
+        # 'managerId': all_managers[random.randint(0, len(all_managers) - 1)].id,
     }
     # all_person_ids = [member.id for member in all_people]
     # group['person_ids'] = random.sample(
@@ -113,6 +112,39 @@ def member_object_factory(sqla):
     }
     return member
 
+# def manager_object_factory(sqla, description, next_level=None, locale_code='en-US'):
+#     """Cook up a fake person."""
+#     description_i18n = f'manager.description.{description.replace(" ", "_")}'[:32]
+# 
+#     if not sqla.query(I18NLocale).get(locale_code):
+#         sqla.add(I18NLocale(code=locale_code, desc='English US'))
+# 
+#     if not sqla.query(I18NKey).get(description_i18n):
+#         i18n_create(description_i18n, 'en-US',
+#                     description, description=f"Manager {description}")
+# 
+#     all_persons = sqla.query(Person).all()
+#     # all_accounts = sqla.query(Account).all()
+#     # if not all_accounts:
+#     #     create_multiple_accounts(sqla)
+#     #     all_accounts = sqla.query(Account).all()
+# 
+#     manager = {
+# 
+#         'person_id': random.choice(all_persons).id,
+#         'description_i18n': description_i18n
+#     }
+#     all_managers = sqla.query(Manager).all()
+# 
+#     if next_level is not None:
+#         next_level_description_i18n = f'manager.description.{next_level.replace(" ", "_")}'
+#         next_level_managers = sqla.query(Manager).filter(Manager.description_i18n == next_level_description_i18n).all()
+#         if (len(next_level_managers) > 0):
+#             manager['manager_id'] = random.choice(next_level_managers).id
+# 
+#     return manager
+
+
 
 def attendance_object_factory(meeting_id, member_id):
     """Cook up a fake attendance json object from given ids."""
@@ -131,16 +163,30 @@ def role_object_factory(role_name):
     }
     return role
 
+def group_type_object_factory(group_type_name):
+    """Cook up a fake group type """
+    group_type = {
+        'name': group_type_name
+    }
+    return group_type
+
+def manager_type_object_factory(manager_type_name):
+    """Cook up a fake manager type """
+    manager_type = {
+        'name': manager_type_name
+    }
+    return manager_type
+
 
 # ---------End of Factories
 
 
 def create_multiple_groups(sqla, n):
     """Commit `n` new groups to the database. Return their IDs."""
-    all_managers = sqla.query(Manager).all()
-    if not all_managers:
-        create_multiple_managers(sqla, random.randint(3, 6))
-        all_managers = sqla.query(Manager).all()
+    # all_managers = sqla.query(Manager).all()
+    # if not all_managers:
+    #     create_multiple_managers(sqla, random.randint(3, 6))
+    #     all_managers = sqla.query(Manager).all()
     group_schema = GroupSchema()
     new_groups = []
     for i in range(n):
@@ -164,10 +210,10 @@ def create_multiple_meetings(sqla, n):
 def create_multiple_members(sqla, n):
     """Commit `n` new members to the database. Return their IDs."""
     member_schema = MemberSchema()
-    all_managers = sqla.query(Manager).all()
-    if not all_managers:
-        create_multiple_managers(sqla, random.randint(3, 6))
-        all_managers = sqla.query(Manager).all()
+    # all_managers = sqla.query(Manager).all()
+    # if not all_managers:
+    #     create_multiple_managers(sqla, random.randint(3, 6))
+    #     all_managers = sqla.query(Manager).all()
     if not sqla.query(Group).all():
         create_multiple_groups(sqla, random.randint(3, 6))
     new_members = []
@@ -184,6 +230,17 @@ def create_multiple_members(sqla, n):
             new_members.append(Member(**valid_member))
             sqla.add(member)
             sqla.commit()
+
+
+# def create_multiple_managers(sqla, n, next_level=None):
+#     """Commit `n` new people to the database. Return their IDs."""
+#     manager_schema = ManagerSchema()
+#     new_managers = []
+#     for i in range(n):
+#         valid_manager = manager_schema.load(manager_object_factory(sqla, fake.sentences(nb=1)[0], next_level))
+#         new_managers.append(Manager(**valid_manager))
+#     sqla.add_all(new_managers)
+#     sqla.commit()
 
 
 def create_attendance(sqla, fraction=0.75):
@@ -219,10 +276,33 @@ def create_role(sqla):
     return valid_role_row.id
 
 
+def create_multiple_group_types(sqla, n):
+    """Commit `n` new group types to the database."""
+    group_type_schema = GroupTypeSchema()
+    new_group_types = []
+    for i in range(n):
+        valid_group_type = group_type_schema.load(group_type_object_factory(" ".join(fake.words(2))))
+        new_group_types.append(GroupType(**valid_group_type))
+    sqla.add_all(new_group_types)
+    sqla.commit()
+
+def create_multiple_manager_types(sqla, n):
+    """Commit `n` new manager types to the database."""
+    manager_type_schema = ManagerTypeSchema()
+    new_manager_types = []
+    for i in range(n):
+        valid_manager_type = manager_type_schema.load(manager_type_object_factory(" ".join(fake.words(2))))
+        new_manager_types.append(ManagerType(**valid_manager_type))
+    sqla.add_all(new_manager_types)
+    sqla.commit()
+
+
 def create_group_test_data(sqla):
     """The function that creates test data in the correct order """
-    create_multiple_groups(sqla, 18)
-    create_multiple_meetings(sqla, 12)
-    create_multiple_members(sqla, 13)
-    create_attendance(sqla, 0.75)
+    create_multiple_group_types(sqla, 5)
+    create_multiple_manager_types(sqla, 5)
+    # create_multiple_groups(sqla, 18)
+    # create_multiple_meetings(sqla, 12)
+    # create_multiple_members(sqla, 13)
+    # create_attendance(sqla, 0.75)
 
