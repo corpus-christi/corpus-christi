f70108b80e36d6b92c7f2ba851126a960a37b615
David Deng <daviddenghaotian@163.com>
2020-08-12 17:12:24 -0400

    updated documentation and script about initializing database

--- api/bin/load-all-app-data.sh
+++ api/bin/load-all-app-data.sh
@@ -2,8 +2,6 @@
 
 # Load all the application-wide flask data.
 
-flask app load-locales
-flask app load-countries
-flask app load-languages
-flask app load-roles
-flask app load-attribute-types
+flask app load-all
+flask i18n load es-EC
+flask i18n load en-US
