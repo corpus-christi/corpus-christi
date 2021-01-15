5041679aa82c0b88e6a2eefed54daac7911e977f
Daisy <d8717013@gmail.com>
2021-01-15 13:26:24 -0500

    Fixed formatting

--- ui/src/pages/public/Events.vue
+++ ui/src/pages/public/Events.vue
@@ -21,22 +21,22 @@
           min-width="290px"
           data-cy="start-date-picker"
         >
-        <template v-slot:activator="{ on }">
-          <v-btn v-on="on">
-          <v-text-field
-            v-model="filterStart"
-            v-bind:label="$t('events.start-date')"
-            prepend-icon="event"
-            readonly
-          />
-          
-          </v-btn>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on">
+              <v-text-field
+                v-model="filterStart"
+                v-bind:label="$t('events.start-date')"
+                prepend-icon="event"
+                readonly
+              />
+            </v-btn>
           </template>
           <v-date-picker
             v-model="filterStart"
             @input="showStartDatePicker = false"
             v-bind:locale="currentLanguageCode"
-          />        </v-menu>
+          />
+        </v-menu>
       </v-flex>
 
       <!-- End Date -->
@@ -52,16 +52,15 @@
           min-width="290px"
           data-cy="end-date-picker"
         >
-        <template v-slot:activator="{ on }">
-          <v-btn v-on="on">
-          <v-text-field
-            v-model="filterEnd"
-            v-bind:label="$t('events.end-date')"
-            prepend-icon="event"
-            readonly
-          />
-          
-          </v-btn>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on">
+              <v-text-field
+                v-model="filterEnd"
+                v-bind:label="$t('events.end-date')"
+                prepend-icon="event"
+                readonly
+              />
+            </v-btn>
           </template>
           <v-date-picker
             v-model="filterEnd"
