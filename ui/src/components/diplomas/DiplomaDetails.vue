701ef79e43dc994634d2250c663b55e38652da56
SecondPsycho <cordell_king@taylor.edu>
2021-01-22 10:44:17 -0500

    WIP: DO NOT PULL

--- ui/src/components/diplomas/DiplomaDetails.vue
+++ ui/src/components/diplomas/DiplomaDetails.vue
@@ -8,7 +8,11 @@
         ><v-icon>arrow_back</v-icon>{{ $t("actions.back") }}</v-btn
       >
     </v-flex>
+    <!--
     <v-flex xs12 sm8 offset-sm2>
+    -->
+    <!-- The Card to the left with the Name and Description of the Diploma -->
+    <v-flex sm12 md3>
       <v-card>
         <template v-if="loading">
           <v-container fill-height fluid>
@@ -18,35 +22,66 @@
           </v-container>
         </template>
         <template v-else>
-          <v-layout row>
-            <v-flex xs12>
-              <v-card>
-                <v-toolbar primary>
-                  <v-toolbar-title>
-                    {{ $t("diplomas.diploma") }}: {{ diploma.name }}
-                  </v-toolbar-title>
-                </v-toolbar>
-                <v-list three-line>
-                  <v-subheader> {{ diploma.description }} </v-subheader>
-                  <v-subheader
-                    >{{ $t("diplomas.courses-this-diploma") }}:</v-subheader
-                  >
-                  <template v-for="(course, index) in diploma.courseList">
-                    <v-list-item :key="index">
-                      <v-list-item-content>
-                        <v-list-item-title
-                          v-html="course.name"
-                        ></v-list-item-title>
-                        <v-list-item-subtitle
-                          v-html="course.description"
-                        ></v-list-item-subtitle>
-                      </v-list-item-content>
-                    </v-list-item>
-                  </template>
-                </v-list>
-              </v-card>
-            </v-flex>
-          </v-layout>
+            <!-- <v-toolbar-title> -->
+            <v-card-title class="d-block">
+              <h5 class="headline">{{ $t("diplomas.diploma") }}: {{ diploma.name }}</h5>
+            </v-card-title>
+            <!-- </v-toolbar-title> -->
+            <v-subheader> {{ diploma.description }} </v-subheader>
+            <!-- Lists the "Courses for this Diploma"
+            <v-subheader>{{ $t("diplomas.courses-this-diploma") }}:</v-subheader>
+            -->
+        </template>
+      </v-card>
+    </v-flex>
+
+    <!-- The tabs responsible for listing the Courses and Students xs12 sm8 -->
+    <v-flex sm12 md9 class="pl-2" v-if="!loading">
+      <v-card>
+        <template>
+          <!-- Tab Headers -->
+          <v-tabs v-model="tab" color="primary" slider-color="accent">
+            <v-tab ripple data-cy="roles-table-tab">
+              <v-icon>school</v-icon>
+              &nbsp;{{ $t("courses.course") }}
+            </v-tab>
+            <v-tab ripple data-cy="roles-table-tab">
+              <v-icon>people</v-icon>
+              &nbsp;{{ $t("people.title") }}
+            </v-tab>
+          </v-tabs>
+          <!-- The Tabs Themselves -->
+          <v-tabs-items v-model="tab">
+            <v-tab-item>
+              <!-- Serially written Course List (TODO: Replace with Table) -->
+              <v-layout row>
+                <v-flex xs12>
+                  <v-list three-line>
+                    <template v-for="(course, index) in diploma.courseList">
+                      <v-list-item :key="index">
+                        <v-list-item-content>
+                          <v-list-item-title
+                            v-html="course.name"
+                          ></v-list-item-title>
+                          <v-list-item-subtitle
+                            v-html="course.description"
+                          ></v-list-item-subtitle>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </template>
+                  </v-list>
+                </v-flex>
+              </v-layout>
+            </v-tab-item>
+            <v-tab-item>
+              <v-flex md11 class="pl-2" v-if="!loading">
+              <!--
+              <v-flex sm12 md9 class="pl-2" v-if="!loading">
+              -->
+                <DiplomaCoursesTable :course="course" />
+              </v-flex>
+            </v-tab-item>
+          </v-tabs-items>
         </template>
       </v-card>
     </v-flex>
@@ -54,9 +89,13 @@
 </template>
 
 <script>
+import DiplomaCoursesTable from "./DiplomaCoursesTable";
+
 export default {
   name: "DiplomaDetails",
-  components: {},
+  components: {
+    DiplomaCoursesTable,
+  },
   props: {
     diplomaId: {
       type: [String, Number],
@@ -68,6 +107,7 @@ export default {
       diploma: {},
       loading: true,
       loadingFailed: false,
+      tab: null,
     };
   },
   mounted() {
--- ui/src/pages/Diplomas.vue
+++ ui/src/pages/Diplomas.vue
@@ -1,10 +1,37 @@
 <template>
-  <v-container> <router-view></router-view> </v-container>
+  <v-container>
+    <!--
+    <v-tabs v-model="tab" color="primary" slider-color="accent">
+      <v-tab ripple data-cy="person-table-tab">
+        <v-icon>person</v-icon>
+        &nbsp;{{ $t("people.title") }}
+      </v-tab>
+      <v-tab ripple data-cy="roles-table-tab">
+        <v-icon>supervisor_account</v-icon>
+        &nbsp;{{ $t("people.title-roles") }}
+      </v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tab">
+      <v-tab-item>
+        <router-view></router-view>
+      </v-tab-item>
+      <v-tab-item>
+      </v-tab-item>
+    </v-tabs-items> 
+    -->
+    <router-view></router-view>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: "Diplomas",
+  /*
+  data() {
+    return {
+      tab: null,
+    };
+  },//*/
 };
 </script>
 
